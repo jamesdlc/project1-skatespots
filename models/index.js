d801a6193577f9cb2fde52b99493cdc24f10e109
jamestothe <jamesdelacruz@gmail.com>
2016-07-22 13:59:35 -0700

    heroku is now working

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 var mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost/skatespots");
 module.exports.Skatespot = require("./skatespots.js");
 
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/skatespots" );
