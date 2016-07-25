5818dbbaa91a40ba252cea772588978fdfe918a4
jamestothe <jamesdelacruz@gmail.com>
2016-07-25 13:26:12 -0700

    added page jumps

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,56 +1,53 @@
-$(document).ready(function(){
-  //AJAX
-  $.get('/api/skatespots', onSuccess);
-  navBar();
+$(document).ready(function() {
+    //AJAX
+    $.get('/api/skatespots', onSuccess);
+    // navBar();
 
-  $('.create-spot-form form').on("submit", function(e){
-    e.preventDefault();
-    console.log("is this button firing");
-    var formData = $(this).serialize();
-    console.log(formData);
-  $.post('/api/skatespots', formData, function(skatespotdata){
-    console.log("created new skatespot", skatespotdata);
-    renderSkatespot(skatespotdata);
-    $(this).trigger("reset");
-  });
-
-});
-
-
+    $('.create-spot-form form').on("submit", function(e) {
+        e.preventDefault();
+        console.log("is this button firing");
+        var formData = $(this).serialize();
+        console.log(formData);
+        $.post('/api/skatespots', formData, function(skatespotdata) {
+            console.log("created new skatespot", skatespotdata);
+            renderSkatespot(skatespotdata);
+            $('.create-spot-form form').trigger("reset");
+        });
+    });
 });
 
 
 function renderSkatespot(skatespot) {
-  var skatespotHtml = $('#spots').html();
-  var skatespotTemplate = Handlebars.compile(skatespotHtml);
-  var html = skatespotTemplate(skatespot);
-  $('#skatespot').prepend(html);
+    var skatespotHtml = $('#spots').html();
+    var skatespotTemplate = Handlebars.compile(skatespotHtml);
+    var html = skatespotTemplate(skatespot);
+    $('#skatespot').prepend(html);
 }
 
 function onSuccess(json) {
 
-  json.forEach(function(skatespot) {
-    renderSkatespot(skatespot);
-    // console.log(skatespot);
-  });
+    json.forEach(function(skatespot) {
+        renderSkatespot(skatespot);
+        // console.log(skatespot);
+    });
 }
 
-function navBar(){
-  var $button1 = $('.allskatespots');
-  var $button2 = $('.addskatespot');
-  var $button3 = $('.myskatespot');
-  var $button4 = $('.loginregister');
+function navBar() {
+    var $button1 = $('.allskatespots');
+    var $button2 = $('.addskatespot');
+    var $button3 = $('.myskatespot');
+    var $button4 = $('.loginregister');
 
-  $button1.click(function() {
-    window.location = "http://localhost:3000/api/skatespots";
-  });
-  $button2.click(function() {
-    window.location = "http://localhost:3000/api/skatespots";
-  });
-  $button3.click(function() {
-    window.location = "http://localhost:3000/api/skatespots";
-  });
-  $button4.click(function() {
-    window.location = "http://localhost:3000/api/skatespots";
-  });
+    $button1.click(function() {
+        window.location = "http://localhost:3000/api/skatespots";
+    });
+    $button2.click(function() {
+        window.location = "http://localhost:3000/api/skatespots";
+    });
+    $button3.click(function() {
+        window.location = "http://localhost:3000/api/skatespots";
+    });
+    $button4.click(function() {
+        window.location = "http://localhost:3000/api/skatespots";
+    });
 }
